22d8fcda5cd51685be9884ed9029f455be9af6c3
spartawwy <32713201+spartawwy@users.noreply.github.com>
2018-03-01 00:09:50 +0800

    edit proj for release

---
+++ include/gloakm_capture_api.h
@@ -0,0 +1,14 @@
+#ifndef GLOAKM_CAPTURE_API_SDSF9045_H_
+#define GLOAKM_CAPTURE_API_SDSF9045_H_
+
+#include <windows.h>
+
+#define DllExport __declspec(dllexport)
+
+typedef int (*ReCallFunc)(void);
+
+extern "C" DllExport BOOL WINAPI InstallLaunchEv(ReCallFunc func, char *tag_str);
+extern "C" DllExport void WINAPI UnInstallLaunchEv();
+
+
+#endif //GLOAKM_CAPTURE_API_SDSF9045_H_
